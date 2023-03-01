dbf12d296dde57d5a6eac722f3e7ae641db55083
B.OKELLO <bokello2010@gmail.com>
2023-03-01 07:16:28 +0300

    event driven add to cart

--- MMM-ECommerce/src/app/services/cart.service.ts
+++ MMM-ECommerce/src/app/services/cart.service.ts
@@ -29,6 +29,8 @@ export class CartService {
       {
         if(tempCartItem.id === theCartItem.id){
           existingCartItem = tempCartItem;
+          existingCartItem.quantity++;
+
           break;
         }
       }
@@ -36,12 +38,52 @@ export class CartService {
       // check if we found it
       alreadyExistsInCart = (existingCartItem != undefined);
 
+       if(!alreadyExistsInCart)
+       {
+          this.cartItems.push(theCartItem);
+       }
+
+      // compute cart total price and total quantity
+
+      this.computeCartTotals();
 
     }
-
-
-
      
+  }
+
+  computeCartTotals() {
+     
+      let totalPriceValue : number = 0;
+      let totalQuantityValue: number = 0;
+
+      for(let currentCartItem of this.cartItems)
+      {
+         totalPriceValue += currentCartItem.quantity * currentCartItem.unitPrice;
+         totalQuantityValue += currentCartItem.quantity;
+      }
+
+      // publish the new values to all subscribers
+
+      this.totalPrice.next(totalPriceValue);
+      this.totalQuantity.next(totalQuantityValue);
+
+      // log cart data for debugging
+      this.logCartData(totalPriceValue, totalQuantityValue);
+
+  }
+
+  logCartData(totalPriceValue: number, totalQuantityValue: number) {
+  
+      console.log(`contents of the cart`);
+
+      for(let tempCartItem of this.cartItems)
+      {
+         const subTotalPrice = tempCartItem.quantity * tempCartItem.unitPrice;
+         console.log(`name= ${tempCartItem.name} quantity= ${tempCartItem.quantity} unitPrice= ${tempCartItem.unitPrice}  subTotalPrice ${subTotalPrice} `);
+      }
+
+     console.log(`totalPrice: ${totalPriceValue.toFixed(2)}, totalQuantity: ${totalQuantityValue}`);
+     console.log(`-------------------------------`);
   }
 
 }
